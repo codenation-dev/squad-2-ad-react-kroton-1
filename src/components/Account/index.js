3b8fac5dc294639857046d630dd94e3620c60e66
heavybr <matheus.cumpian@hotmail.com>
2020-01-14 01:51:26 -0200

    Auth and Routes with firebase

---
+++ src/components/Account/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+const AccountPage = () => (
+  <div>
+    <h1>Account Page</h1>
+    <PasswordChangeForm />
+  </div>
+);
+export default AccountPage;
