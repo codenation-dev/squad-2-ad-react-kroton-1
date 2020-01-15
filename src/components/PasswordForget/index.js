d66117258c605e753e76ecb4b83956552231e33d
heavybr <matheus.cumpian@hotmail.com>
2020-01-15 19:59:29 -0200

    Implementando contextos de autenticação e autorização e criando os respectivos high order components

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+
+
 const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
     <PasswordForgetForm />
   </div>
 );
+
 const INITIAL_STATE = {
   email: '',
   error: null,
 };
+
 class PasswordForgetFormBase extends Component {
   constructor(props) {
     super(props);
     this.state = { ...INITIAL_STATE };
   }
+
   onSubmit = event => {
     const { email } = this.state;
     this.props.firebase
@@ -29,9 +34,11 @@ class PasswordForgetFormBase extends Component {
       });
     event.preventDefault();
   };
+
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
   render() {
     const { email, error } = this.state;
     const isInvalid = email === '';
@@ -52,11 +59,13 @@ class PasswordForgetFormBase extends Component {
     );
   }
 }
+
 const PasswordForgetLink = () => (
   <p>
     <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
   </p>
 );
+
 export default PasswordForgetPage;
 const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
 export { PasswordForgetForm, PasswordForgetLink };
